82b2d394f0305ca98fd50c081f7047b76197329a
christopherkolodziej <krzysztof.kolodziej0@gmail.com>
2025-05-10 16:30:27 +0200

    minor refactor

--- wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
+++ wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
@@ -1,6 +1,6 @@
 package com.jpacourse.persistance.dao;
 
-import com.jpacourse.dto.PatientTO;
+//import com.jpacourse.dto.PatientTO;
 import com.jpacourse.persistance.entity.PatientEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
